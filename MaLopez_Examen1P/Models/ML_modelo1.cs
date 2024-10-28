f8afa31eebaefd2f624dfba3a9aebdccef3f9548
laboratorio <laboratorio@UPEAULA-31708>
2024-10-28 10:38:11 -0500

    correccion base datos

--- MaLopez_Examen1P/Models/ML_modelo1.cs
+++ MaLopez_Examen1P/Models/ML_modelo1.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MaLopez_Examen1P.Models
 {
@@ -8,12 +9,13 @@ namespace MaLopez_Examen1P.Models
         public int Id { get; set; }
         [Required]
         [StringLength(20, ErrorMessage = "El nombre no puede exceder los 100 caracteres")]
+        
         public string? Nombre { get; set; }
         public int Edad { get; set; }
         public string? Direccion { get; set; }
         [Required]
         public string? Departamento { get; set; }
-        public DateTime? FechaNacimiento { get; set; }
+        //public DateTime FechaNacimiento { get; set; }
         public bool IsCliente { get; set; }
         [Range(0, 1000000)]
         public decimal Saldo { get; set; }
