81925cf618e5fc21d0b52c5523d3cf2926afee3c
laboratorio <laboratorio@UPEAULA-31708>
2024-10-28 09:57:46 -0500

    Agregar archivos de proyecto.

---
+++ MaLopez_Examen1P/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MaLopez_Examen1P.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
